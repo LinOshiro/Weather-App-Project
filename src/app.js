b1bbba7f93c76f69fb95497e10dfb1837ddfb1f0
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-04-11 15:05:12 -0500

    Added relevant icon from the api

--- src/app.js
+++ src/app.js
@@ -3,34 +3,50 @@ function formatDate(timestamp) {
     let hours = date.getHours();
     if (hours < 10) {
         hours = `0${hours}`;
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
     }
+    let minutes = date.getMinutes();
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
         
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesay", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
-    return `${day} ${hour}:${minute}`;
-}
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesay",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+        let day = days[date.getDay()];
+        return `${day} ${hours}:${minutes}`;
+    }
 
 
-function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let dateElement = document.querySelector("#date");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-}
+    function displayTemperature(response) {
+        let temperatureElement = document.querySelector("#temperature");
+        let cityElement = document.querySelector("#city");
+        let descriptionElement = document.querySelector("#description");
+        let humidityElement = document.querySelector("#humidity");
+        let windElement = document.querySelector("#wind");
+        let dateElement = document.querySelector("#date");
+        let iconElement = document.querySelector("#icon");
 
-let apiKey = "358bb59892afa5069bcb43f658651551";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&appid=${apiKey}&units=imperial`;
+        temperatureElement.innerHTML = Math.round(response.data.main.temp);
+        cityElement.innerHTML = response.data.name;
+        descriptionElement.innerHTML = response.data.weather[0].description;
+        humidityElement.innerHTML = response.data.main.humidity;
+        windElement.innerHTML = Math.round(response.data.wind.speed);
+        dateElement.innerHTML = formatDate(response.data.dt * 1000);
+        iconElement.setAttribute("src",
+            `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+        iconElement.setAttribute("alt", response.data.weather[0].description);
+    }
+
+    let apiKey = "358bb59892afa5069bcb43f658651551";
+    let city = "Los Angeles";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     
     
-axios.get(apiUrl).then(displayTemperature);
+    axios.get(apiUrl).then(displayTemperature);
